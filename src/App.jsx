0417c315f318ccd21adddf99f2eb1871cc5c5d30
sourov-chandra <sourovtpi@yahoo.com>
2026-04-01 22:10:41 +0600

    update basic things

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Suspense, useState } from "react";
+import "./App.css";
+import Banner from "./components/banner/Banner";
+import Navbar from "./components/navbar/Navbar";
+import Products from "./components/products/Products";
+import StatsBar from "./components/statistics/StatsBar";
+import Steps from "./components/steps/Step";
+import Pricing from "./components/price/Pricing";
+import CTA from "./components/cta/CTA";
+import Footer from "./components/footer/Footer";
+
+const fetchProducts = async () => {
+  const res = await fetch("./productData.json");
+  return res.json();
+};
+
+const fetchPricing = async () => {
+  const res = await fetch("./pricing.json");
+  return res.json();
+};
+
+const productsPromise = fetchProducts();
+const pricingPromise = fetchPricing();
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cart, setCart] = useState([]);
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+      <header>
+        <Navbar cartCount={cart.length} />
+      </header>
+      <main>
+        <Banner />
+        <StatsBar />
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center w-full h-full">
+              <span className="loading loading-ring loading-xl"></span>
+            </div>
+          }
         >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+          <Products
+            productsPromise={productsPromise}
+            cart={cart}
+            setCart={setCart}
+          />
+        </Suspense>
+        <Steps />
+        <Suspense
+          fallback={
+            <div className="flex items-center justify-center w-full h-full">
+              <span className="loading loading-ring loading-xl"></span>
+            </div>
+          }
+        >
+          <Pricing pricingPromise={pricingPromise} />
+        </Suspense>
+        <CTA />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
